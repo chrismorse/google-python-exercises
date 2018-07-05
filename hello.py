4057b7893997bf67b5d9c615354b8e8d1f4c42c6
chrismorse <github@beeksma.com>
2018-07-05 19:16:21 +0100

    string1 exercises

--- hello.py
+++ hello.py
@@ -22,19 +22,17 @@ import sys
 # Define a main() function that prints a little greeting.
 def main():
 
-  print(sys.argv)
-
   # Get the name from the command line, using 'World' as a fallback.
   if len(sys.argv) >= 2:
     name = sys.argv[1]
   else:
     name = 'World'
-  print ('Hello', name)
-  christest()
+
+  christest(name)
 
 
-def christest():
-  print('sup')  
+def christest(name):
+  print('Hello', name)  
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
